8d99da1a5d4e19c07350fe9f26a45eda7c15fe6d
Amanda S <amanda.santi@live.com>
2023-10-23 23:23:17 -0500

    404

---
+++ app/src/components/NotFound/NotFound.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import notFoundImage from '../../assets/images/notfound.png';
+import styles from './NotFound.module.css';
+
+export const NotFound = (props) => {
+  const [hasError, setHasError] = useState(false);
+
+  if(hasError){
+    return (
+      <div className={styles.container}>
+        <div>
+          <img src={notFoundImage} alt="404 not found" />
+        </div>
+        
+        <div className={styles.column}>
+          <h1>404: Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+        </div>
+      </div>
+    );
+  }
+
+  try{
+    return this.props.children;
+  }
+  catch (error) {
+    setHasError(true);
+    return null;
+  }
+}

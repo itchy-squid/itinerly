e9f2fd8112dbe6969517e1ed12ae9d3cd3890d96
Amanda Santi <86495347+itchy-squid@users.noreply.github.com>
2023-10-24 22:41:42 -0500

    a little styling on the projects page (#19)

--- app/src/views/ProjectsView/ProjectsView.js
+++ app/src/views/ProjectsView/ProjectsView.js
@@ -1,9 +1,9 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
+import styles from './ProjectsView.module.css';
 import { useNavigate } from 'react-router-dom';
 import { useProjects } from '../../contexts/ProjectsContext';
-import { Box, Fab, LinearProgress, List, ListItemButton, ListItemText, TextField } from '@mui/material';
+import { Box, Button, Fab, Grid, LinearProgress, TextField, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export const ProjectsView = () => {
   const { projects, loading } = useProjects();
@@ -22,36 +22,89 @@ export const ProjectsView = () => {
 
   if (loading) return <LinearProgress />;  // Assuming you have a Loading component
 
-  return (
-    <Box p={2}>
-      <Box mb={2}>
-        <TextField
-          fullWidth
-          label="Search Projects"
-          variant="outlined"
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-        />
-      </Box>
+  const xsSize = 12;
+  const smSize = 6;
+  const mdSize = 4;
+  const lgSize = 3
 
-      <List>
-      {filteredProjects.map(({id, name, description}) => (
-        <Fragment key={id}>
-          <ListItemButton onClick={() => handleProjectClick(id)}>
-            <ListItemText primary={name} secondary={description}/>
-            <ChevronRightIcon/>
-          </ListItemButton>
-        </Fragment>
-      ))}
-      </List>
-      
-      <Fab 
-        color="primary" 
-        aria-label="add" 
-        sx={{ m: 4 }}
-        style={{ position: 'fixed', bottom: 0, right: 0 }} >
-        <AddIcon />
-      </Fab>
+  return (
+    <Box p={0}>
+      <Box className={styles.projectsBanner} 
+        sx={{ height: { xs: '200px', md: '300px', lg: '450px' } }}
+        mb={2}
+        />
+
+      <Grid container rowSpacing={4}
+        my={2}>
+        {(filteredProjects.length > 4) && (
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Search Projects"
+              variant="outlined"
+              value={searchTerm}
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </Grid>
+        )}
+
+        <Grid item xs={xsSize} sm={smSize} md={mdSize} lg={lgSize}>
+          <Box className={[styles.projectCardContents,styles.recentProjectsCardContents]}
+                mx={3}
+                mb={3}
+                mt={0}>
+                
+            <Box className={styles.projectCardHeader}>
+              <Typography variant='h5' component="div">
+                Recent Projects
+              </Typography>
+            </Box>
+
+            <Box className={styles.recentProjectsCardBody}>
+            </Box>
+          </Box>
+        </Grid>
+
+        {filteredProjects.map(({id, name, description}) => (
+          <Grid item key={id} className={styles.projectCard} 
+            xs={xsSize}
+            sm={smSize}
+            md={mdSize}
+            lg={lgSize}>
+
+            <Box className={styles.projectCardContents}
+              mx={3}
+              mb={3}
+              mt={0}>
+
+              <Box className={styles.projectCardHeader} 
+                px={2}
+                mb={2}>
+                <Typography variant='h5' component="div">{name}</Typography>
+              </Box>
+
+              <Box className={styles.projectCardBody}
+                p={2}
+              >
+                <Typography variant="body2" color="text.secondary">
+                  {description}
+                </Typography>
+                <Button size="small" onClick={() => handleProjectClick(id)}>
+                  Continue
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+        
+        <Fab 
+          color="primary" 
+          aria-label="add" 
+          sx={{ m: 4 }}
+          style={{ position: 'fixed', bottom: 0, right: 0 }} >
+          <AddIcon />
+        </Fab>
+      </Grid>
     </Box>
   );
 }

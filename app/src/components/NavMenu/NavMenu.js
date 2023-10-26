44bc28f027c3abbf623c31ce8da4b5b1cdf3774b
Amanda S <amanda.santi@live.com>
2023-10-26 06:41:14 -0500

    app bar first pieces

--- app/src/components/NavMenu/NavMenu.js
+++ app/src/components/NavMenu/NavMenu.js
@@ -3,6 +3,8 @@ import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from '
 import { Link } from 'react-router-dom';
 import { navRoutes } from '../../AppRoutes.js';
 import './NavMenu.css';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -25,6 +27,22 @@ export class NavMenu extends Component {
   render() {
     return (
       <header>
+        <Box>
+          <AppBar position='static'>
+            <Toolbar>
+              <IconButton size='small'
+                edge='start'
+                color='inherit'
+                aria-label='menu'
+                sx={{mr: 2}}>
+                <MenuIcon/>
+              </IconButton>
+              <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                itinerly
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow" container light>
           <NavbarBrand tag={Link} to="/">itinerly</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
